0cb9381b82606f2ed267dcd2eb965e7dc02f23d0
daeun <leede0418@likelion.org>
2020-02-07 01:45:41 +0900

    fix: Add the missing variable temp

--- Weather.js
+++ Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-export default function Weather() {
+export default function Weather({ temp }) {
     return (
         <View style={styles.container}>
             <Text>{temp}</Text>
